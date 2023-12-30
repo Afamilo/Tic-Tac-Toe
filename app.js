022dc536cab74f707b23a4576735a0c3cbc21256
Afamilo <nichomartins@gmail.com>
2023-12-30 00:05:20 -0600

    created a game function

--- app.js
+++ app.js
@@ -46,5 +46,53 @@ const createPlayer = (name, mark) => {
     }
 }
 
+const game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, 'X'),
+            createPlayer(document.querySelector('#player2').value, 'O'),
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+
+    const handleclick = (event) => {
+        if(gameOver){
+            return;
+        }
+        let index = parseInt(event.target.id.split('-')[1]);
+        if(Gameboard.getGameboard()[index] !=='')
+            return;
+        Gameboard.update(index, players[currentPlayerIndex].mark)
+
+        if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+            gameOver = true;
+            displayController.displayMessage(`${players[currentPlayerIndex].name} won!`)   
+        }else if(checkForTie(Gameboard.getGameboard())){
+            gameOver = true;
+            displayController.displayMessage(`it's a tie!`)
+        }
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1: 0;
+    }
+    const restart = () => {
+        for  (let i = 0; i < 9; i++) {
+            Gameboard.update(i, '');
+        }
+        Gameboard.render();
+        gameOver = false;
+        document.querySelector('#message').innerHTML = '';
+    }
+    return {
+        start,
+        restart,
+        handleclick
+    }
+})();
+
 
 
